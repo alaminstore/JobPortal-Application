aba4754f0d1b62aadcd5c29ad6ff73c6ba41f8ed
Sm Al-amin <67338203+alaminstore@users.noreply.github.com>
2020-09-18 20:03:52 +0600

    Create CompanyController.php

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    public function index($id,Company $company){
+        return view('company.index',compact('company'));
+    }
+}
