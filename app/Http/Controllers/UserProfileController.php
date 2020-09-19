a04c2d5b1182d9542ca625b3df82f7134de7ccbe
Sm Al-amin <67338203+alaminstore@users.noreply.github.com>
2020-09-20 05:19:31 +0600

    Update UserProfileController.php

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -15,14 +15,18 @@ class UserProfileController extends Controller
            'dob'=>'required',
            'gender'=>'required',
            'address'=>'required',
+           'phone'=>'required|regex:/(01)[0-9](9)/',
            'bio'=>'required|min:20',
+           'experience'=>'required',
         ]);
         $user_id = auth()->user()->id;
         Profile::where('user_id',$user_id)->update([
             'dob'=>request('dob'),
             'gender'=>request('gender'),
             'address'=>request('address'),
+            'phone'=>request('phone'),
             'bio'=>request('bio'),
+            'experience'=>request('experience'),
         ]);
         return redirect()->back()->with('message','Profile updated successfully');
     }
@@ -55,6 +59,9 @@ class UserProfileController extends Controller
 
 
     public function avatar(Request $request){
+        $this->validate($request,[
+            'avatar' =>'required|mimes:jpg,jpeg,png|max:1024',
+        ]);
         $user_id = auth()->user()->id;
         if ($request->hasFile('avatar')){
             $file = $request->file('avatar');
