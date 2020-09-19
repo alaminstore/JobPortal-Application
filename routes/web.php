f4eeefbf8cbdb4f1b42215a9ba4b3bbe342c94ee
Sm Al-amin <67338203+alaminstore@users.noreply.github.com>
2020-09-19 23:36:44 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,19 @@ use App\Http\Controllers\UserProfileController;
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+//job
 Route::get('/',[JobController::class,'index']);
 Route::get('jobs/{id}/{job}',[JobController::class,'show'])->name('jobs.show');
+
+//company
 Route::get('company/{id}/{company}',[CompanyController::class,'index'])->name('company.index');
+
+//userProfile
 Route::get('user/profile', [UserProfileController::class, 'index']);
 Route::post('profile/store',[UserProfileController::class, 'store'])->name('profile.store');
 Route::post('profile/coverletter',[UserProfileController::class, 'coverletter'])->name('profile.coverletter');
 Route::post('profile/resume',[UserProfileController::class, 'resume'])->name('profile.resume');
+Route::post('profile/avatar',[UserProfileController::class, 'avatar'])->name('profile.avatar');
 
 Auth::routes();
 
