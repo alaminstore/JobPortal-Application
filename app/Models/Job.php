647f9567c2b57e1a1d4b2a8a32c65ccd2d08563b
Sm Al-amin <67338203+alaminstore@users.noreply.github.com>
2020-09-24 17:21:55 +0600

    Existing id find and recheck for applicant

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,15 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\DB;
 class Job extends Model
 {
+    protected $guarded=[];
     public function getRouteKeyName(){
         return 'slug';
     }
 
     public function company(){
         return $this->belongsTo(Company::class);
+    }
+
+    public function users(){
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
+
+    public function checkApply(){
+        return DB::table('job_user')->where('user_id', auth()->user()->id)
+            ->where('job_id',$this->id)->exists();
     }
 
     use HasFactory;
