3767d04503356adecf787fb08b0131bcfde95c2c
Sm Al-amin <67338203+alaminstore@users.noreply.github.com>
2020-09-24 17:23:10 +0600

    Update show.blade.php

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -23,16 +23,35 @@
                 <div class="card">
                     <div class="card-header">Short Information</div>
                     <div class="card-body">
-                        <p>Company: {{$job->company->cname}}</p>
+                        <a href="{{route('company.index',[$job->company->id,$job->company->slug])}}">
+                            <p>Company: {{$job->company->cname}}</p>
+                        </a>
                         <p>Address: {{$job->address}}</p>
                         <p>Job Position: {{$job->position}}</p>
                         <p>DeadLine: {{$job->last_date}}</p>
                     </div>
-                    @if(Auth::check())
-                    <button class="btn btn-secondary  btn-block btn-sm">Apply The Job</button>
+                    @if(Session::has('message'))
+                        <div class="alert alert-success">
+                            <i class="fa fa-check-square-o" aria-hidden="true"></i>  {{Session::get('message')}}
+                        </div>
+                    @endif
+
+
+                    @if(Auth::check() && Auth::user()->user_type=='seeker')
+                        @if(!$job->checkApply())
+                            <form action="{{route('jobs.apply',[$job->id] )}}" method="post">
+                                @csrf
+                                <button class="btn btn-secondary  btn-block btn-sm">Apply The Job</button>
+                            </form>
+                        @else
+                            <p class=" text-center alert alert-"><b>Applied</b></p>
+                        @endif
+                    @else
+                        <p class=" text-center alert alert-warning"><b>Login to apply</b></p>
                     @endif
                 </div>
             </div>
         </div>
     </div>
 @endsection
+{{--auth()->user()->id--}}
